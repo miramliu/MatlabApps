cb1a1bd615a53733bc857121bb2eb10ad1336550
Mira L <30845365+miramliu@users.noreply.github.com>
2022-04-25 12:41:28 -0500

    Average IVIM curve
    added plots and changed visuals for proposal

--- AverageIVIMCurve.m
+++ AverageIVIMCurve.m
@@ -1,11 +1,22 @@
 %put explanation here please 
 
+
+
+% Example Run: 
+%GMroi = '/Users/neuroimaging/Desktop/DATA/IVIM_TimTests/MIRA_IVIM/gmROItest.mat';
+%WMroi = '/Users/neuroimaging/Desktop/DATA/IVIM_TimTests/MIRA_IVIM/wmROItest.mat';
+%CSFroi = '/Users/neuroimaging/Desktop/DATA/IVIM_TimTests/MIRA_IVIM/csfROItest.mat';
+%IVIMfile = '/Users/neuroimaging/Desktop/DATA/IVIM_TimTests/MIRA_IVIM/Scan 02 IVIM_S0D3_sorted_2step.mat';
+%ImageDirectory = '/Users/neuroimaging/Desktop/DATA/IVIM_TimTests/MIRA_IVIM/Scan 02 IVIM_S0D3_sorted';
+%Slice = 20;
+%ROIdirect = '/Users/neuroimaging/Desktop/DATA/IVIM_TimTests/MIRA_IVIM/csfROItest.mat';
+%>> AverageIVIMCurve(IVIMfile,ImageDirectory,Slice,CSFroi,WMroi,GMroi)
 %mira liu 04/11/2022
 
 function AverageIVIMCurve(varargin)
 
 ivimfilepath = varargin{1};
-load(varargin{1}, 'f', 'D', 'Dstar') %this is the IVIM file
+load(ivimfilepath, 'f', 'D', 'Dstar') %this is the IVIM file
 Image_Directory=varargin{2}; %the path to the directory
 slice = varargin{3};
 ROI_directory = varargin{4}; 
@@ -31,6 +42,7 @@ Start_Index = 28;
 
 %this is path to either 1) roi saved as mat file that you want you want 
 % or 2) roi that you're going to draw and save here.
+%The input would be AverageIVIMCurve(IVIMfile,ImageDirectory,Slice,ROI_Name)
 if nargin == 4
     if ~exist(ROI_directory,'file')
         figure,imshow(Image,[0 120]),colormap(gca,'jet'),colorbar,truesize([300 300])
@@ -47,10 +59,14 @@ if nargin == 4
     hold on
     scatter(Bvalues,Signal/Signal(1),70,"black","filled")
     hold off
+
+    CSF_sig = 0;
+    WM_sig = 0;
+    GM_sig = 0;
 end
 
 
-%put in directories for CSF, wm, and gm (in this order)
+%The input would be AverageIVIMCurve(IVIMfile,ImageDirectory,Slice,CSF_path, WM_path, GM_path)
 if nargin == 6
     CSF_roi = varargin{4};
     load(CSF_roi,'ROI') %load
@@ -66,21 +82,54 @@ if nargin == 6
     GM_sig = getAvergeROI(Image_Directory,Images_Per_Slice,Start_Index,nx,ny,Num_Bvalues,datnames,slice,GM);
 
     %plots (normalized)
-    norm_to = 1; %(either normalized to b0, or normalize to b1000)
+    norm_to = 1; %( normalized to b0)
     figure,
-    plot(Bvalues,CSF_sig/CSF_sig(norm_to),LineWidth=3.0,Color='blue')
+    plot(Bvalues,CSF_sig/CSF_sig(norm_to),LineWidth=3.0,Color=[0.9290 0.6940 0.1250]) %CSF is yelow
     hold on
-    scatter(Bvalues,CSF_sig/CSF_sig(norm_to),70,"blue","filled")
+    scatter(Bvalues,CSF_sig/CSF_sig(norm_to),70,markerfacecolor = "black")
     hold on
-    plot(Bvalues,WM_sig/WM_sig(norm_to),LineWidth=3.0,Color='green')
+    plot(Bvalues,WM_sig/WM_sig(norm_to),LineWidth=3.0,Color=[0.4660 0.6740 0.1880]) %WM is green
     hold on
-    scatter(Bvalues,WM_sig/WM_sig(norm_to),70,"green","filled")
+    scatter(Bvalues,WM_sig/WM_sig(norm_to),70,markerfacecolor = "black")
     hold on
-    plot(Bvalues,GM_sig/GM_sig(norm_to),LineWidth=3.0,Color='red')
+    plot(Bvalues,GM_sig/GM_sig(norm_to),LineWidth=3.0,Color=[0 0.4470 0.7410]) %GM is purple
     hold on
-    scatter(Bvalues,GM_sig/GM_sig(norm_to),70,"red","filled")
+    scatter(Bvalues,GM_sig/GM_sig(norm_to),70,markerfacecolor = "black")
     hold off
     legend('CSF','','WM','','GM','')
+
+    norm_to = 10; %normalize to b1000=10
+    figure,
+    plot(Bvalues,CSF_sig/CSF_sig(norm_to),LineWidth=3.0,Color=[0.9290 0.6940 0.1250]) %CSF is yelow
+    hold on
+    scatter(Bvalues,CSF_sig/CSF_sig(norm_to),70,markerfacecolor = "black")
+    hold on
+    plot(Bvalues,WM_sig/WM_sig(norm_to),LineWidth=3.0,Color=[0.4660 0.6740 0.1880]) %WM is green
+    hold on
+    scatter(Bvalues,WM_sig/WM_sig(norm_to),70,markerfacecolor = "black")
+    hold on
+    plot(Bvalues,GM_sig/GM_sig(norm_to),LineWidth=3.0,Color=[0 0.4470 0.7410]) %GM is purple
+    hold on
+    scatter(Bvalues,GM_sig/GM_sig(norm_to),70,markerfacecolor = "black")
+    hold off
+    legend('CSF','','WM','','GM','')
+
+    %no nonrm
+    figure,
+    plot(Bvalues,CSF_sig,LineWidth=3.0,Color=[0.9290 0.6940 0.1250]) %CSF is yelow
+    hold on
+    scatter(Bvalues,CSF_sig,70,markerfacecolor = "black")
+    hold on
+    plot(Bvalues,WM_sig,LineWidth=3.0,Color=[0.4660 0.6740 0.1880]) %WM is green
+    hold on
+    scatter(Bvalues,WM_sig,70,markerfacecolor = "black")
+    hold on
+    plot(Bvalues,GM_sig,LineWidth=3.0,Color=[0 0.4470 0.7410]) %GM is purple
+    hold on
+    scatter(Bvalues,GM_sig,70,markerfacecolor = "black")
+    hold off
+    legend('CSF','','WM','','GM','')
+    xlabel('b-value (s/mm^2)',FontSize=25)
 end
 
 
